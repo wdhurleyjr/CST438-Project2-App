43975022057cc892cf5f17e8c89e2392d87e0a7a
aharo1110 <aharo1110@gmail.com>
2024-10-13 16:24:14 -0700

    Start with page title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,14 @@ import React from 'react';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <View style={styles.centeredTitleContainer}>
+        <Text style={styles.logoTitle}>
+          Bestsellers
+        </Text>
+        <Text style={styles.welcomeTitle}>
+          Welcome, user!
+        </Text>
+      </View>
     </View>
   );
 }
@@ -20,5 +27,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f0',
     padding: 25,
     marginTop: 25,
-},
+  },
+  centeredTitleContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+    paddingHorizontal: 20,
+  },
+  logoTitle: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Judson-bold', // NEED TO FIX !!!
+    color: '#000',
+    lineHeight: 50,
+  },
+  welcomeTitle: {
+    fontSize: 20,
+    lineHeight: 40,
+  },
 });
